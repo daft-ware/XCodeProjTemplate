b8fb8c464a93c2d7740c1ff91abc799e98367437
scraptechguy <rostislav.bro@gmail.com>
2021-11-13 11:40:19 +0100

    Initial Commit

---
+++ <your project name>/_your_project_name_App.swift
@@ -0,0 +1,17 @@
+//
+//  _your_project_name_App.swift
+//  <your project name>
+//
+//  Created by Rostislav Brož on 11/13/21.
+//
+
+import SwiftUI
+
+@main
+struct _your_project_name_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ <your project name>Tests/_your_project_name_Tests.swift
@@ -0,0 +1,33 @@
+//
+//  _your_project_name_Tests.swift
+//  <your project name>Tests
+//
+//  Created by Rostislav Brož on 11/13/21.
+//
+
+import XCTest
+@testable import _your_project_name_
+
+class _your_project_name_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
